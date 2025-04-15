d12d04e32e68371f7c5c73d953e7f390b9cd3133
Rodrigo Torres Rivera <rodrigo.torres4234@alumnos.udg.mx>
2025-04-15 03:42:30 -0600

    Se agregan nuevos programas

---
+++ GUI/ToplevelWidgest.py
@@ -0,0 +1,16 @@
+
+from tkinter import *
+root = Tk()
+#root.option_readfile('optionDB')
+root.title('Toplevel')
+Label(root, text='This is the main (default) Toplevel').pack(pady=10)
+t1=Toplevel(root)
+Label(t1, text='This is a child of root0').pack(padx=10, pady=10)
+t2=Toplevel(root)
+Label(t2,text='This is a trasciente window of root').pack(padx=10, pady=10)
+t2.transient(root)
+t3=Toplevel(root,borderwidth=5, bg='blue')
+Label(t3,text='No wm decorations', bg='blue', fg='white').pack(padx=10, pady=10)
+t3.overrideredirect(1)
+t3.geometry('200x70+150+150')
+root.mainloop() #important to showt the GUI
--- OOP/README.md
+++ OOP/README.md
@@ -1,1 +1,3 @@
 Esta carpeta contiene ejemplos de OOP
+Se han agregado los archivos ClasessInFunctions ObjectOrientedPrograming ObjectsModues
+Archivo SecretString.py
